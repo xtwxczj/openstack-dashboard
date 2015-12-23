b42e5a1086495d67b92dd8d70f4f16b1c6e130c6
xtwxczj <xtwxczj@sina.com>
2015-12-23 08:33:46 -0500

    hi

--- manage.py
+++ manage.py
@@ -21,3 +21,4 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                           "openstack_dashboard.settings")
     execute_from_command_line(sys.argv)
+
